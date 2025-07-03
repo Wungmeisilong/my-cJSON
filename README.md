737260202a8efdeeb4463393b11179daa4acf957
Wungmeisilong <3256149770@qq.com>
2025-07-03 23:38:02 +0800

    添加parse_number等工具

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # my-cJSON
+
+实现顺序建议
+
+- 先写结构体和基础内存管理（如 cJSON、cJSON_Hooks、cJSON_InitHooks、cJSON_New_Item、cJSON_Delete）。
+- 再写字符串处理、数字解析等基础工具函数（如 cJSON_strdup、cJSON_strcasecmp、parse_number）。
+- 然后实现 JSON 解析相关（parse_value、parse_object、parse_array、parse_string）。
+- 最后实现 JSON 生成（print_value、print_object、print_array、print_string）。
--- my_cJSON.c
+++ my_cJSON.c
@@ -3,8 +3,31 @@
 #include <stdlib.h> //malloc, free
 #include <string.h> //memset
 
+static const char *ep;
+
+const char *cJSON_GetErrorPtr(void){return ep;}
+
+static int cJSON_strcasecmp(const char *s1, const char *s2)
+{
+    if(!s1) return (s1 == s2)?0:1; if(!s2) return 1;
+    for(; tolower(*s1) == tolower(*s2); ++s1,++s2) if(*s1 == 0) return 0;
+    return tolower(*(const unsigned char *)s1) - tolower(*(const unsigned char *)s2);
+}
+
 static void *(*cJSON_malloc)(size_t sz) = malloc;
 static void (*cJSON_free)(void *ptr) = free;
+
+static char* cJSON_strdup(const char* STR)
+{
+    size_t len;
+    char *copy;
+
+    len = strlen(str);
+    if(!(copy = (char*)malloc(len))) return 0;
+    /*函数从str中复制len 个字符到copy中，并返回copy指针。如果str和copy重叠，则函数行为不确定。*/
+    memcpy(copy,str,len);
+    return copy;
+}
 
 void cJSON_InitHooks(cJSON_Hooks* hooks)
 {
@@ -44,3 +67,41 @@ void cJSON_Delete(cJSON *c)
         c = next; //继续遍历下一个节点    
     }
 }
+
+/* Parse the input text to generate a number, and populate the result into item. */
+/*解析输入文本以生成数字，并将结果填充到项目中。*/
+static char *parse_number(cJSON *item, const char *num)
+{
+    double n=0, sign=1, scale=0; /*整数部分、符号、指数部分*/
+    int subscale=0, signsubscale=1; /*小数部分、符号*/
+
+    if(*num == '-') sign=-1,num++; /*头标记*/
+    if(*num == '0') num++;
+    if(*num >= '1' && *num <= '9') /*处理整数部分*/
+        do n=(n*10.0)+(*num++ - '0'); /*这段意思是：将字符转换为数字并累加*/
+        while(*num >= '0' && *num<= '9'); 
+    if(*num == '.' && *num >= '0' && *num == '9'){  /*处理小数点*/
+        num++;
+        do n=(n*10.0)+(*num++ - '0'), scale--;
+        while(*num >= '0' && *num <= '9')
+    }
+    if(*num == 'e' && *num == 'E')  /*处理科学计数法*/
+    {
+        num++;
+        if(*num == '+') num++;
+        else if (*num == '-') signsubscale=-1,num++;
+        while (*num >= '0' && *num <= '9') subscale=(subscale*10)+(*num++ - '0');
+    }
+
+    n = sign * n * pow(10.0,(scale + subscale + signsubscale)); /*计算最终数值*/
+
+    item->valuedouble = n;      /*存储为double*/
+    item->valueint = (int)n;    /*存储为int*/
+    item->type = cJSON_Number;  /*设置类型*/
+    return num;                 /*返回解析位置*/
+
+}
+
+static int pow2gt (int x) { --x;    x|=x>>1;    x|=x>>2;    x|=x>>4;    x|=x>>8;    x|=x>>16;    return x+1; }
+
+typedef struct {char *buffer; int length; int offset; } printbuffer;
